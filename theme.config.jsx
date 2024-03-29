17ac138c0f16d31dca3fbe09e4953b0118a15295
G. A. 02 <blessedbyhope05@gmail.com>
2024-03-29 13:20:39 +0700

    commit

---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Why can't you multiply vectors? - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Why can't you multiply vectors?" />
+          <meta property="og:description" content="Join Freya Holmér on a captivating journey into the realm of vector multiplication and geometric algebra. This talk explores the fundamental question of why vectors can't be multiplied directly and unravels the enigmatic world of complex numbers, quaternions, and bivectors. Through a series of thought-provoking examples and engaging explanations, Freya demonstrates how the axiom of vector squared length leads to the emergence of familiar products and constructs in mathematics. Prepare to have your perception of vector multiplication transformed as you discover the elegant beauty of geometric algebra and its profound implications for game development and beyond." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
